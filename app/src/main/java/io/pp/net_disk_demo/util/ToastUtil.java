9056aec2415c4011440408546666b4d0791137bd
huangyuan0pplabs <44431235+huangyuan0pplabs@users.noreply.github.com>
2019-02-18 01:51:29 +0800

    Add try catch for toat

--- app/src/main/java/io/pp/net_disk_demo/util/ToastUtil.java
+++ app/src/main/java/io/pp/net_disk_demo/util/ToastUtil.java
@@ -3,6 +3,7 @@ package io.pp.net_disk_demo.util;
 import android.content.Context;
 import android.view.Gravity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dovar.dtoast.DToast;
 import com.dovar.dtoast.inner.IToast;
@@ -15,15 +16,20 @@ public class ToastUtil {
 
     public static void showToast(Context context, String text, int time) {
 
-        if (toast == null) {
-            toast = DToast.make(context);
-        }
+        try {
+            if (toast == null) {
+                toast = DToast.make(context);
+            }
 
-        TextView tv_text = (TextView) toast.getView().findViewById(R.id.tv_content);
-        if (tv_text != null) {
-            tv_text.setText(text);
-        }
+            TextView tv_text = (TextView) toast.getView().findViewById(R.id.tv_content);
+            if (tv_text != null) {
+                tv_text.setText(text);
+            }
 
-        toast.setGravity(Gravity.CENTER, 0, 30).show();
+            toast.setGravity(Gravity.CENTER, 0, 30).show();
+        } catch (Exception e) {
+            Toast.makeText(context, text, time);
+            e.printStackTrace();
+        }
     }
 }
